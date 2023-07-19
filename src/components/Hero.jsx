ae1b4bb8c003252ff3e9093275b9f0ba03552720
Charan1303 <charan.cherry1303@gmail.com>
2023-07-19 19:48:10 -0230

    made it responsive, fixed the nav bar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,10 +3,17 @@ import {logo} from '../assets';
 const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
-      <nav className='flex justify-between items-center
-       flex-col'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
         <img src={logo} alt="sumz_logo" 
         className='w-28 object-contain'/>
+
+        <button
+          type="button"
+          onClick={() => window.open('https://https://github.com/Charan1303/ai_summarizer')}
+          className='black_btn'
+          >
+          Github 
+        </button>
       </nav>
       
       </header>
