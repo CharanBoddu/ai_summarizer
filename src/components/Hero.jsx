79ee7f7117bcdb80fca87a42be411ad4e64deca9
Charan1303 <charan.cherry1303@gmail.com>
2023-07-19 19:50:53 -0230

    making it mobile accesible

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
       </nav>
 
       <h1 className='head_text'>
-        Summarize Articles with <br />
+        Summarize Articles with <br className='max-md:hidden'/>
         <span className='orange_gradient'>OpenAI GPT-4</span>
       </h1>
       
