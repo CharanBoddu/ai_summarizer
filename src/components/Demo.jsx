f962cd3f267f7dcdfcaa4053eda3735494f9c883
Charan1303 <charan.cherry1303@gmail.com>
2023-07-19 20:11:50 -0230

    modifying the input text bar

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -2,12 +2,21 @@ import { useState, useEffect } from "react";
 import {copy, linkIcon, loader, tick} from "../assets";
 
 const Demo = () => {
+  const [article, setArticle] = useState({
+    url: '',
+    summary: '',
+
+  });
+
+  const handleSubmit = async (e) => {
+    alert('Submitted');
+  }
   return (
     <section className="mt-16 w-full max-w-xl">
       {/* Search */}
       <div className="flex flex-col w-full gap-2">
       <form className="relative flex justify-center items-center"
-        onSubmit={() => {}}
+        onSubmit={handleSubmit}
       >
         <img 
         src={linkIcon}
@@ -19,8 +28,10 @@ const Demo = () => {
         <input 
           type="url"
           placeholder="Enter a URL"
-          value=""
-          onChange={() => {}}
+          value={article.url}
+          onChange={(e) => setArticle({ ... article, url: e.target.value
+
+          })}
           required
           className="url_input peer"
         
@@ -34,7 +45,11 @@ const Demo = () => {
           ↵ 
         </button>
       </form>
+
+      {/* Browser URL History */}
       </div>
+
+      {/* Display Results */}
     </section>
   )
 }
