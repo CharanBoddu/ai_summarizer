bd9fab629ed68d221a75662bdacad7c2f9e4a5d2
Charan1303 <charan.cherry1303@gmail.com>
2023-07-19 19:44:08 -0230

    changing the text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import {logo} from '../assets';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-between items-center
+       flex-col'>
+        <img src={logo} alt="sumz_logo" 
+        className='w-28 object-contain'/>
+      </nav>
+      
+      </header>
   )
 }
 
