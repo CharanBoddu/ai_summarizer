875c8930bed9f6c4161ce08ea4e4ddd987406002
Charan1303 <charan.cherry1303@gmail.com>
2023-07-20 20:50:14 -0230

    changing font

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,8 +4,10 @@ const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-        <img src={logo} alt="sumz_logo" 
-        className='w-28 object-contain'/>
+      <div className='flex items-center'>
+      <img src={logo} alt="sumz_logo" className='w-28 object-contain' />
+      <h1 className='head2_text mb-8 pl-5'>C Sumz</h1>
+    </div>
 
         <button
           type="button"
