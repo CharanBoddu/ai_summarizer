53c1c1af79eaba6804ca2492bf6cd6c933147949
Charan1303 <charan.cherry1303@gmail.com>
2023-10-25 07:04:30 -0230

    checking

--- src/services/article.js
+++ src/services/article.js
@@ -1,6 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 
 
 
@@ -9,7 +8,7 @@ export const articleApi = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
         prepareHeaders: (headers) => {
-            headers.set('X-RapidAPI-Key', rapidApiKey);
+            headers.set('X-RapidAPI-Key', import.meta.env.VITE_RAPID_API_ARTICLE_KEY);
             headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
 
             return headers;
